bd196b198fdc7d1a5d4b979d49a3444e57d1a7ab
uroshkarabash <101325785+uroskarabas@users.noreply.github.com>
2023-06-12 14:49:31 +0200

    Delete OOP_N03_PR01 directory

--- OOP_N03_PR01/src/rs/ac/singidunum/igre/oop/Lav.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.singidunum.igre.oop;
-
-public class Lav extends Neprijatelj {
-    public Lav(double x, double y, double ugaoR, String slika, double brzina, String naziv, double zivot, double snaga) {
-        super(x, y, ugaoR, slika, brzina, naziv, zivot, snaga);
-    }
-}
